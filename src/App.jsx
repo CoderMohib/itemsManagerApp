5d977c68846ef9d0f88c086b69565eaf4a6ec9a0
Mohib Ali <alimohib025@gmail.com>
2025-02-03 22:15:49 +0500

    SideBar Work Done and some useConext

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import SideBar from "./Components/Organism/SideBar";
-import NavProvider from "./Components/NavContext";
 import "./styles/index.css";
 function App() {
   return (
-    <NavProvider>
       <SideBar />
-    </NavProvider>
   );
 }
 export default App;
--- src/Components/Atoms/NavItem.jsx
+++ src/Components/Atoms/NavItem.jsx
@@ -1,9 +1,4 @@
-import { useContext } from "react"
-import { NavContext } from "../NavContext"
+
 export default function NavItem({icon, text}){
-    const [ activePage, setActivePage ] = useContext(NavContext);
-    const handleClick = () => {
-        setActivePage(page);
-      };
-    return <a href="#" className="nav-item" onClick={handleClick}>{icon} {text}</a>
+    return <a href="#" className="nav-item">{icon} {text}</a>
 }
