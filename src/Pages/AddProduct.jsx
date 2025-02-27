62226c723fa60a24fa77af6e71cb3bcba63c8835
Mohib Ali <alimohib025@gmail.com>
2025-02-27 19:23:30 +0500

    Approx remove all bugs

--- src/Pages/AddProduct.jsx
+++ src/Pages/AddProduct.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../styles/form.css";
 import { products as InitialProducts } from "../data/product";
 import AddProductForm from "../Components/Organism/AddProductForm";
 export default function AddProduct() {
   const [products, setProducts] = useState(InitialProducts);
-  useEffect(() => {
-    console.log("Updated products:", products);
-  }, [products]);
   function addProduct(newProduct) {
-        setProducts((prevProd) => [...prevProd, { id: prevProd.length+1, ...newProduct }]);
+    setProducts((prevProd) => [
+      ...prevProd,
+      { id: prevProd.length + 1, ...newProduct },
+    ]);
   }
 
   return <AddProductForm addProduct={addProduct} />;
